65957aa5b1144874bbf023ba7375515d09cc0fa4
sajaffrey <146390349+sajaffrey@users.noreply.github.com>
2024-03-12 10:57:36 -0400

    a commit

---
+++ assets/main/main.js
@@ -0,0 +1,9 @@
+// Your custom JavaScript using third-party libraries
+    // Example: Initialize Slick slider
+    $(document).ready(function(){
+        $('.slider').slick({
+          dots: true,
+          autoplay: true,
+          autoplaySpeed: 1000
+        });
+      });
